ee383e5ef97b5c32df78e5ea83b3f978cb0f106c
Rebecca Sandberg <rebeccasandberg02@gmail.com>
2025-12-30 16:33:49 +0100

    Button function
    All button function is made to be able to use the calculator.

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const valuesForButtons = [
 const topSymbols = ["AC", "+/-", "%"];
 const mathSymbols = ["÷", "x", "-", "+", "="];
 
+const display = document.getElementById("display");
+display.value = "";
+
+let A = 0;
+let B = null;
+let operator = null;
+
+function clearAll() {
+    A = 0;
+    B = null;
+    operator = null;
+}
 
 for (let i = 0; i < valuesForButtons.length; i++) {
     let value = valuesForButtons[i];
@@ -26,6 +38,59 @@ for (let i = 0; i < valuesForButtons.length; i++) {
         button.style.backgroundColor = "#CC66DA";
         button.style.color = "#000000";
     }
+
+    button.addEventListener("click", function () {
+        if (mathSymbols.includes(value)) {
+            if (value == "=") {
+                if (A != null) {
+                    B = display.value;
+                    let numA = Number(A);
+                    let numB = Number(B);
+
+                    if (operator == "÷") {
+                        display.value = numA / numB;
+                    } else if (operator == "x") {
+                        display.value = numA * numB;
+                    } else if (operator == "-") {
+                        display.value = numA - numB;
+                    } else if (operator == "+") {
+                        display.value = numA + numB;
+                    }
+
+                    clearAll();
+                }
+            } else {
+                operator = value;
+                A = display.value;
+                display.value = "";
+            }
+        } else if (topSymbols.includes(value)) {
+            if (value == "AC") {
+                clearAll();
+                display.value = "";
+            } else if (value == "+/-") {
+                if (display.value != "" && display.value != "0") {
+                    if (display.value[0] == "-") {
+                        display.value = display.value.slice(1);
+                    } else {
+                        display.value = "-" + display.value;
+                    }
+                }
+            } else if (value == "%") {
+                display.value = Number(display.value) / 100;
+            }
+        } else if (display.value == "0") {
+            display.value = value;
+        } else {
+            if (value == ".") {
+                if (display.value != "" && !display.value.includes(value)) {
+                    display.value += value;
+                }
+            } else {
+                display.value += value;
+            }
+        }
+    })
 
     let buttonsDiv = document.getElementById("buttons");
     buttonsDiv.appendChild(button);
